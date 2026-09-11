f1c9819185fa0017a633587b389bc55570e37774
RONAK PANDEY <134931454+RONAK-PANDEY@users.noreply.github.com>
2026-09-11 18:48:17 +0530

    Create Ouestion 5.py

---
+++ Ouestion 5.py
@@ -0,0 +1,4 @@
+obtained_marks = float(input("Enter the obtained marks: ").strip())
+Total_marks = float(input("Enter the total marks: ").strip())
+
+print(f'percentage: {(obtained_marks / Total_marks) * 100:.2f}')
